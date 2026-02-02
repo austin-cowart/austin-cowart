159cb62deb3449b4a62146703c3d12cac78c708a
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-02-02 17:36:07 +0000

    finished M1T1

--- M1T1.cpp
+++ M1T1.cpp
@@ -8,8 +8,13 @@ cowarta
 #include <iostream>
 using namespace std;
 
-
 int main(){
+    cout << "Hello, user!" << endl; // this is "endline"
+    cout << "What's your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you " << name << endl;
+    
     return 0;
 }
 
