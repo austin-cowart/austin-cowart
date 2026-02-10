a4887a47e1e0d86dfd54c94aff401c239c282dd7
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-02-10 00:34:17 +0000

    Started M2T1

---
+++ M2T1.cpp
@@ -0,0 +1,28 @@
+//CSC 134
+//M2T1 - Product Sales      
+// Austin Cowart
+// 02/09/26
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Set up all Variables
+    string first_name, last_name, full_name; // holds customer name
+    string product = "apples"; //change to whatever you like
+    double cost_each = 0.99;
+    double total_cost;
+
+
+
+    // Greet the Customer
+    cout << "Welcome to our " << product << " store!" << endl;
+
+
+    //Ask how much they'd like to purchase
+
+    //Calculate Total Price
+    // Give the Result
+
+}
