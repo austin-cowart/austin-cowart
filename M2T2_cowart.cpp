9106962adaf3000cc6371d97468fdd6d4d727d97
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-02-17 03:41:12 +0000

    Started M2T2

---
+++ M2T2_cowart.cpp
@@ -0,0 +1,16 @@
+/*
+CSC-134
+M2T2 - Reciept Calculator
+cowarta
+02/17/2026
+Goal: A correct looking receipt that handles sales tax. 
+Assumption: Sales tax is 8% (varies by county)
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+}
