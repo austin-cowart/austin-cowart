b2f1aafb1dcb5ca274b96a6fa4a78b44627b8a7e
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-03-04 02:24:07 +0000

    Finished M3LAB2

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -6,25 +6,50 @@ M3LAB2
 */
 
 // Convert a Number Grade (0-100)
-// To a Letter Grade (A-F)
+// To a Letter Grade (F-A)
 
 #include <iostream>
 using namespace std;
 
 int main(){
+
+    // Determine Letter Grade
+    // Given Numerical Grade Value
     
-    // Ask for the numerical grade
-    int numerical_grade;
-    cout << "What is your grade?: ";
+    // Ask for the numerical grade from User
+    int numerical_grade; // The Numerical Grade of User
+    cout << "Enter your numerical grade: ";
     cin >> numerical_grade;
 
-    // Determine Letter Grade within each grade range
-    // Given Numerical Grade Value
-    if (numerical_grade > 3){
-        cout << "F";
+    // Variable to Display Letter Grade After Calculation
+    string letter_grade; // Letter Grade of User
+
+    // Calculate Letter Grade based off Grade Ranges
+    // Starting from Grade F(0-59) to Grade A(90-100)
+    if (numerical_grade >= 0 && numerical_grade <= 59) {
+
+        letter_grade = "F";
+
+    } else if (numerical_grade >= 60 && numerical_grade <= 69) {
+
+        letter_grade = "D";
+
+    } else if (numerical_grade >= 70 && numerical_grade <= 79) {
+        
+        letter_grade = "C";
+
+    } else if (numerical_grade >= 80 && numerical_grade <= 89) {
+
+        letter_grade = "B";
+
+    } else if (numerical_grade >= 90 && numerical_grade <= 100) {
+
+        letter_grade = "A";
+
     }
 
+    // Display Letter Grade to User
+    cout << "Your letter grade is: " << letter_grade << endl;
 
-
-    return 0;
+    return 0; // No Errors
 }
