620e157db7563bcdc5d2ab968e6c46b3e9003278
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-03-12 05:22:52 +0000

    Started M3HW

---
+++ M3HW.cpp
@@ -0,0 +1,28 @@
+/*
+
+CSC 134
+M3HW1 - Gold
+Austin Cowart
+03/12/2026
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+void Question_One(){
+    // Question 1
+    cout << "Question 1" << endl;
+    
+    // Asks user how is their day
+    string response;
+    
+
+}
+
+int main(){
+    Question_One();
+    return 0;
+}
