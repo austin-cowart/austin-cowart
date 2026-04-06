9131ca69e23617d1e2fc25a0c3e00f4da321496b
austin-cowart <cowarta6834@student.faytechcc.edu>
2026-04-06 20:25:20 +0000

    Finished M5LAB1

--- M5LAB1.cpp
+++ M5LAB1.cpp
@@ -18,12 +18,14 @@ void choice_turn_back();
 void choice_enter_forest();
 
 int main() {
-    cout << "Game Title" << endl;
+    cout << "Four Paths" << endl;
     // Load the Main Menu
     main_menu();
     // We are done when we return here.
+    cout << "-------------" << endl;
     cout << "Thanks for Playing!" << endl;
-    return 0; // No Errors
+    cout << "-------------" << endl;
+    return 0; // Finish with No Errors
 }
 
 void main_menu() {
@@ -34,19 +36,21 @@ void main_menu() {
     cout << "-------------" << endl;
     cout << "You're in the middle of a four-way crossroad." << endl;
     cout << "Would you like to:" << endl;
-    cout << " 1. Travel forwards towards a river" << endl;
+    cout << "1. Travel forwards towards a river" << endl;
     cout << "2. Go right into a cave" << endl;
     cout << "3. Go left into the forest" << endl;
     cout << "4. Turn back the way you came" << endl;
     cout << "5. [Quit]" << endl;
-    cout << "Choice: " << endl;
+    cout << "-------------" << endl;
+    cout << "Choice: ";
     int choice;
     cin >> choice;
+    cout << "-------------" << endl;
     if (choice == 1) {
         choice_cross_river();
     } else if (choice == 2) {
         choice_enter_cave(); 
-    } else if (choice == 3) 
+    } else if (choice == 3) {
         choice_enter_forest();
     } else if (choice == 4) {
         choice_turn_back();
@@ -54,10 +58,56 @@ void main_menu() {
         cout << "Quitting Game..." << endl;
         return; // Goes back to main()
     } else {
+        cout << "-------------" << endl;
         cout << "That is not a valid choice. Please try again." << endl;
+        cout << "-------------" << endl;
         cin.ignore(); // Clear user input
-        main_menu();
+        main_menu(); // Try Again
     }
 
 }   
 
+// Choice 1 - Cross the River
+void choice_cross_river() { 
+    cout << "-------------" << endl;
+    cout << "Crossing the River" << endl;
+    cout << "-------------" << endl;
+    cout << "You sucessfully managed to cross the river," << endl;
+    cout << "And came across a village!" << endl;
+    cout << "-------------" << endl;
+    return;
+}
+
+// Choice 2 - Enter the Cave
+void choice_enter_cave() { 
+    cout << "-------------" << endl;
+    cout << "Entering the Cave" << endl;
+    cout << "-------------" << endl;
+    cout << "You walked into the deep, dark cave," << endl;
+    cout << "and came across a bear!" << endl;
+    cout << "-------------" << endl;
+    return;
+}
+
+// Choice 3 - Enter the Forest
+void choice_enter_forest() {
+    cout << "-------------" << endl;
+    cout << "Entering the Forest" << endl;
+    cout << "-------------" << endl;
+    cout << "You enter the thriving forest," << endl;
+    cout << "and found lots of fruits, berries and wildlife!" << endl;
+    cout << "You don't know which berries are edible, though." << endl;
+    cout << "-------------" << endl;
+    return;
+}
+
+// Choice 4 - Turn Back
+void choice_turn_back() {
+    cout << "-------------" << endl;
+    cout << "Turning Back" << endl;
+    cout << "-------------" << endl;
+    cout << "You decide to go back," << endl;
+    cout << "not knowing what could await you." << endl;
+    cout << "-------------" << endl;
+    return;
+}
